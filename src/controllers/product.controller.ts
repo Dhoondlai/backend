2473d2070dbe4d4f9fd37eb9c422c960dfbc5cea
okHadi <khan.hadi2951@gmail.com>
2025-04-28 23:30:03 +0500

    feat: improved fetch all products

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -71,60 +71,64 @@ export const getAllProducts = async (req: Request, res: Response) => {
     const skip = (page - 1) * limit;
 
     // First count unique standard_names for total count
-    const uniqueProductsCount = await collections.products?.distinct('standard_name').then(arr => arr.length);
+    const uniqueProductsCount = await collections.products
+      ?.distinct("standard_name")
+      .then((arr) => arr.length);
 
     // Fetch products grouped by standard_name with vendor count and best price
-    const products = await collections.products?.aggregate([
-      // Group by standard_name
-      {
-        $group: {
-          _id: "$standard_name",
-          vendors_count: { $sum: 1 },  // Count products with same standard_name
-          // Find the minimum price and corresponding vendor
-          min_price: { $min: "$price_low" },
-          all_products: { 
-            $push: { 
-              vendor: "$vendor", 
-              price_low: "$price_low",
-              category: "$category",
-              available: "$available"
-            } 
-          }
-        }
-      },
-      // Add best price vendor field
-      {
-        $addFields: {
-          best_price_vendor: {
-            $arrayElemAt: [
-              {
-                $filter: {
-                  input: "$all_products",
-                  as: "product",
-                  cond: { $eq: ["$$product.price_low", "$min_price"] }
-                }
+    const products = await collections.products
+      ?.aggregate([
+        // Group by standard_name
+        {
+          $group: {
+            _id: "$standard_name",
+            vendors_count: { $sum: 1 }, // Count products with same standard_name
+            // Find the minimum price and corresponding vendor
+            min_price: { $min: "$price_low" },
+            all_products: {
+              $push: {
+                vendor: "$vendor",
+                price_low: "$price_low",
+                category: "$category",
+                available: "$available",
               },
-              0
-            ]
-          }
-        }
-      },
-      // Project final fields
-      {
-        $project: {
-          _id: 0,
-          standard_name: "$_id",
-          vendors_count: 1,
-          best_price: "$min_price",
-          best_price_vendor: "$best_price_vendor.vendor",
-          category: { $arrayElemAt: ["$all_products.category", 0] },
-          available: { $in: [true, "$all_products.available"] } // Available if any vendor has it
-        }
-      },
-      { $sort: { standard_name: 1 } },
-      { $skip: skip },
-      { $limit: limit }
-    ]).toArray();
+            },
+          },
+        },
+        // Add best price vendor field
+        {
+          $addFields: {
+            best_price_vendor: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: "$all_products",
+                    as: "product",
+                    cond: { $eq: ["$$product.price_low", "$min_price"] },
+                  },
+                },
+                0,
+              ],
+            },
+          },
+        },
+        // Project final fields
+        {
+          $project: {
+            _id: 0,
+            standard_name: "$_id",
+            vendors_count: 1,
+            best_price: "$min_price",
+            best_price_vendor: "$best_price_vendor.vendor",
+            category: { $arrayElemAt: ["$all_products.category", 0] },
+            available: { $in: [true, "$all_products.available"] }, // Available if any vendor has it
+          },
+        },
+        { $sort: { standard_name: 1 } },
+        { $skip: skip },
+        { $limit: limit },
+      ])
+      .toArray();
 
     return res.status(200).send({
       status: 200,
